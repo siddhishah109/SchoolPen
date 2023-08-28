4fdc7ff59e82ef32d63d86323271cd4d07234b6c
Siddhi Shah <shahsiddhi777@gmail.com>
2023-08-28 21:06:00 +0530

    fgsgs

---
+++ src/components/application.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Application = () => {
+  return (
+    <div className='application'>
+      <div className='app'>Application
+      <div className='app1'>
+        <div className='app2'>
+photo
+        </div>
+        <div className='app3'>
+        <input type="text" name="name" className='titl2' placeholder='Add Title' />
+        <div className='con2'>
+        <input type="text" name="name" className='tarea' placeholder='Content' />
+      
+        </div>
+
+        </div>
+      </div>
+      <button className='Addapp'>Add</button>
+      </div>
+      <div className='item'>add item</div>
+    </div>
+  )
+}
+
+export default Application
--- src/components/partA.js
+++ src/components/partA.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Application from './application'
 
 const PartA = () => {
   return (
     <div className='PartA'>
-      ffsdfds
+      
+    
+    <input type="text" name="name" className='title' placeholder='Add Title' />
+    <div className='objective'>
+    <input type="text" name="Objective" className='tarea' placeholder='Objective' />
+    </div>
+    <div className='content'><input type="text" name="Content" className='tarea' placeholder='Content' />
+    </div>
+    <Application/>
     </div>
   )
 }
