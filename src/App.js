71f43dedd055676f78aa979338fb877988ec3130
Siddhi Shah <shahsiddhi777@gmail.com>
2023-08-28 10:34:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Admin from './pages/admin';
+
+function App() {
+  return (
+    <>
+<Admin/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/admin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../components/sideBar'
+import Body from '../components/body'
+
+const Admin = () => {
+  return (
+    <div>
+      <SideBar/>
+      <Body/>
+    </div>
+  )
+}
+
+export default Admin
